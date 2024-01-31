7c1b15b3d3f35033a1527f7321a878573f4faaa9
sumsumcorn <nicecorntea@naver.com>
2024-01-31 23:15:55 +0900

    next/font local 폰트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
 import bg from '@/assets/images/bread1.jpg';
+import { Dancing_Script } from 'next/font/google';
+
+const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
     <>
@@ -13,17 +19,9 @@ export default function Home() {
         style={{ zIndex: -1 }}
       />
       <section className={`${styles.hero}`}>
-        <h1 className={styles.title}>WCB Bakery</h1>
-        <p>
-          엄선된 재료와 식감이 사이 있는 빵.
-          <br />
-          매일 아침 갓 구운 빵.
-          <br />
-          빵과 함께 행복한 시간을 만끽해보세요.
-        </p>
-      </section>
-      <section className={`${styles.hero}`}>
-        <h1>WCB Bakery</h1>
+        <h1 className={`${dancingScript.className} ${styles.title}`}>
+          WCB Bakery
+        </h1>
         <p>
           엄선된 재료와 식감이 사이 있는 빵.
           <br />
