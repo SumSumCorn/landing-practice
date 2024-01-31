332b0341730eb35debe380f4ef6ce296cfc4b5de
sumsumcorn <nicecorntea@naver.com>
2024-01-31 23:25:45 +0900

    아이콘 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
 import bg from '@/assets/images/bread2.jpg';
-import { Dancing_Script } from 'next/font/google';
-
+import { Dancing_Script, Single_Day } from 'next/font/google';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from 'react-icons/fa';
+import { FaMeta } from 'react-icons/fa6';
 const dancingScript = Dancing_Script({
   subsets: ['latin'],
 });
+const singleDay = Single_Day({ weight: '400' });
 
 export default function Home() {
   return (
@@ -20,15 +23,16 @@ export default function Home() {
       />
       <section className={`${styles.hero}`}>
         <h1 className={`${dancingScript.className} ${styles.title}`}>
-          WCB Bakery
+          WEB Bakery
         </h1>
-        <p>
+        <p className={singleDay.className}>
           엄선된 재료와 식감이 사이 있는 빵.
           <br />
           매일 아침 갓 구운 빵.
           <br />
           빵과 함께 행복한 시간을 만끽해보세요.
         </p>
+        <FaXTwitter /> <FaInstagram /> <FaMeta />
       </section>
     </>
   );
